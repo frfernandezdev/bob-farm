eba709931477c8d77db00a90d5889b97040753fa
Fernando Fernandez <frfernandezdev@gmail.com>
2024-12-26 19:34:06 -0400

    fix: bug in shadcn components

--- apps/website/src/modules/shared/infrastructure/shadcn/components/ui/form.tsx
+++ apps/website/src/modules/shared/infrastructure/shadcn/components/ui/form.tsx
@@ -12,8 +12,8 @@ import {
   useFormContext,
 } from "react-hook-form"
 
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import { cn } from "@shadcn/lib/utils"
+import { Label } from "@shadcn/components/ui/label"
 
 const Form = FormProvider
 
